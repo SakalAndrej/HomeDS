ed5f045bb55710bb4e876f9d0e3fa4f35045a3bf
Unknown <felix.hofmann@liwest.at>
2017-10-24 09:43:19 +0200

    New Programm for ESP
    Recieve and Send the IR Codes

---
+++ Reciever-Emitter-Programm-ESP8266/src/main.cpp
@@ -0,0 +1,55 @@
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+#include "IRsend.h"
+
+uint16_t RECV_PIN = 4; //an IR detector connected to D4
+
+IRrecv irrecv(RECV_PIN);
+IRsend irsend(0);
+
+
+decode_results results;
+
+void setup()
+{
+  Serial.println("Setup initiated ...");
+  irsend.begin();
+  Serial.begin(9600);
+  //irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  /*
+  Serial.println("Waiting for IR-Signal ...");
+  if (irrecv.decode(&results)) {
+    for(int i = 0; i < 5; i++) {
+      delay(1000);
+      Serial.print(".");
+    }
+
+    Serial.print("Empfangen: ");
+    serialPrintUint64(results.value, HEX);
+
+
+    irsend.sendLG(results.value);
+      delay(3000);
+    Serial.println("Sent 1st signal");
+    irsend.sendLG(results.value);
+      delay(3000);
+      Serial.println("Sent 2nd signal");
+    irsend.sendLG(results.value);
+      delay(3000);
+      Serial.println("Sent 3rd signal");
+
+
+    Serial.println("i made it 2");
+    irrecv.resume(); // Receive the next value
+}
+*/
+irsend.sendNEC(0x20df10ef, 32);
+delay(5000);
+Serial.print(".");
+
+
+}

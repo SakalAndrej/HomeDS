44cce46237483ca2e392712d62eb6366a49d771e
SakalAndrej <andi.sakal15@gmail.com>
2017-10-25 09:23:38 +0200

    Added LG-Sender Programm

---
+++ LG-Sender/src/main.cpp
@@ -0,0 +1,21 @@
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+#include "IRsend.h"
+
+uint16_t SEND_PIN = 2; //an IR detector connected to D4
+
+IRsend irsend(SEND_PIN);
+
+void setup()
+{
+  Serial.println("Setup initiated ...");
+  irsend.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+irsend.sendNEC(0x20df10ef, 32);
+delay(5000);
+Serial.print(".");
+}

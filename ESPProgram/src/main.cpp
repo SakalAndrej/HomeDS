320e4b839e3970e7765d268eef2e6ffd37b2deb5
SakalAndrej <andi.sakal15@gmail.com>
2017-10-30 15:23:02 +0100

    Auto stash before merge of "master" and "origin/master"

--- ESPProgram/src/main.cpp
+++ ESPProgram/src/main.cpp
@@ -3,34 +3,34 @@
 #include "IRsend.h"
 
 
-  IRsend panasonic(0);
+  IRsend irsender(0);
   //IRsend panasonic64(0);
 
 void setup() {
-  panasonic.begin();
+  irsender.begin();
   Serial.begin(9600);
   Serial.println("START");
 }
 
 void loop() {
 
-
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
-  panasonic.sendPanasonic(0x4004, 0x0190ED7C);
+/*
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);
+  irsender.sendPanasonic(0x4004, 0x0190ED7C);*/
   Serial.print("first");
   delay(1000);
 
-  panasonic.sendPanasonic64(0x40040190ED7C, PANASONIC_BITS, 10);
+  //irsender.sendPanasonic64(0x40040190ED7C, PANASONIC_BITS, 10);
 
   delay(1000);
 

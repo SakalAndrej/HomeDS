2787180141aa52900fc13876f470c29630def3d9
SakalAndrej <andi.sakal15@gmail.com>
2017-12-28 02:00:22 +0100

    testing purposes

--- RemoteXibo/app/src/main/java/at/htl/remotexibo/viewholder/DisplayGroupViewHolder.java
+++ RemoteXibo/app/src/main/java/at/htl/remotexibo/viewholder/DisplayGroupViewHolder.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,12 +21,19 @@ public class DisplayGroupViewHolder extends RecyclerView.ViewHolder {
         tvName = itemView.findViewById(at.htl.remotexibo.R.id.tvName);
         tvDescr = itemView.findViewById(R.id.tvDisplayGroupDescription);
         cb1 = itemView.findViewById(R.id.cbPrimary);
-
     }
+
 
     public void updateUI(DisplayGroup displayGroup){
         tvName.setText(String.format(displayGroup.getDisplayGroupName()));
         tvDescr.setText(String.format(displayGroup.getDisplayGroupDescription()));
         cb1.setChecked(false);
+
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(itemView.getContext(), "Selected: " + displayGroup.displayGroupName, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }

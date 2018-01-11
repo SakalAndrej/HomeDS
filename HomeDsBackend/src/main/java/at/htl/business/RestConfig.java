51dbc1394e6808d37e8999d34db53ab77b662a89
SakalAndrej <andi.sakal15@gmail.com>
2018-01-11 01:22:19 +0100

    fixed swagger core and added swagger ui

--- HomeDsBackend/src/main/java/at/htl/business/RestConfig.java
+++ HomeDsBackend/src/main/java/at/htl/business/RestConfig.java
@@ -14,12 +14,12 @@ public class RestConfig extends Application {
 
     public RestConfig() {
         BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.0");
+        beanConfig.setVersion("1.0.1");
         beanConfig.setTitle("HomeDSBackend");
         beanConfig.setSchemes(new String[]{"http"});
         beanConfig.setHost("localhost:8080");
         beanConfig.setBasePath("/homeds/rs");
-        beanConfig.setResourcePackage("at/htl/rest");
+        beanConfig.setResourcePackage("at.htl.rest");
         beanConfig.setScan(true);
         beanConfig.setPrettyPrint(true);
     }

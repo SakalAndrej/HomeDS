420c0dc7ddbe59773edac229a9c6f479d317a1e0
SakalAndrej <andi.sakal15@gmail.com>
2018-01-17 21:35:48 +0100

    fixed index controller

--- HomeDsBackend/src/main/java/at/htl/web/IndexController.java
+++ HomeDsBackend/src/main/java/at/htl/web/IndexController.java
@@ -3,20 +3,18 @@ package at.htl.web;
 import at.htl.model.DataSet;
 import at.htl.xiboClient.DataSetApi;
 
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Model;
 import javax.inject.Inject;
-import javax.inject.Named;
 import java.io.Serializable;
-import java.util.LinkedList;
+import java.util.List;
 
-@Named
-@RequestScoped
+@Model
 public class IndexController implements Serializable{
 
     @Inject
     DataSetApi dataSetApi;
 
-    public LinkedList<DataSet> getDataSet() {
+    public List<DataSet> getDataSet() {
         return dataSetApi.getAllDataSet(-1,null,null);
     }
 }

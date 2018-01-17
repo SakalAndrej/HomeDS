5aa803c0e5c8951e29b7cba81509adc689071afd
SakalAndrej <andi.sakal15@gmail.com>
2018-01-17 21:36:08 +0100

    dataset controller refactored

---
+++ HomeDsBackend/src/main/java/at/htl/web/DataSetController.java
@@ -0,0 +1,51 @@
+package at.htl.web;
+
+import at.htl.model.DataSet;
+import at.htl.model.DataSetData;
+import at.htl.xiboClient.DataSetApi;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+@Model
+public class DataSetController implements Serializable {
+
+    @Inject
+    DataSetApi dataSetApi;
+
+    private DataSet selectedDataSet;
+
+    public DataSetController() {
+        selectedDataSet = new DataSet();
+    }
+
+    public List<DataSet> getDataSets() {
+        return dataSetApi.getAllDataSet(-1,null,null);
+    }
+
+    public void onSelect(DataSet dataSet, String typeOfSelection, String indexes) {
+        if (null != dataSet) {
+            selectedDataSet = dataSet;
+
+            MessagesController cr = new MessagesController();
+            cr.setMessage(" " + dataSet.getDataSetName() + " wurde ausgewählt");
+            cr.TriggerInfoMessage();
+        }
+    }
+
+    public LinkedList<DataSetData> getDataSetDataById() {
+        LinkedList<DataSetData> f = new LinkedList<>();
+        f = dataSetApi.getAllDataSetData(2);
+
+        return f;
+    }
+
+    //region Getter
+    public DataSet getSelectedDataSets() {
+        return selectedDataSet;
+    }
+    //endregion
+}

c81889fc52c97eb08b4a3d1b843697182c29bd46
SakalAndrej <andi.sakal15@gmail.com>
2018-01-18 14:06:11 +0100

    internationalization

---
+++ HomeDsBackend/src/main/java/at/htl/web/LanguageController.java
@@ -0,0 +1,59 @@
+package com.mkyong;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+
+@ManagedBean(name="language")
+@SessionScoped
+public class LanguageController implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private String localeCode;
+
+    private static Map<String,Object> countries;
+    static{
+        countries = new LinkedHashMap<String,Object>();
+        countries.put("English", Locale.ENGLISH);
+        countries.put("Chinese", Locale.GERMANY);
+    }
+
+    public Map<String, Object> getCountriesInMap() {
+        return countries;
+    }
+
+
+    public String getLocaleCode() {
+        return localeCode;
+    }
+
+
+    public void setLocaleCode(String localeCode) {
+        this.localeCode = localeCode;
+    }
+
+    //value change event listener
+    public void countryLocaleCodeChanged(ValueChangeEvent e){
+
+        String newLocaleValue = e.getNewValue().toString();
+
+        //loop country map to compare the locale code
+        for (Map.Entry<String, Object> entry : countries.entrySet()) {
+
+            if(entry.getValue().toString().equals(newLocaleValue)){
+
+                FacesContext.getCurrentInstance()
+                        .getViewRoot().setLocale((Locale)entry.getValue());
+
+            }
+        }
+    }
+
+}

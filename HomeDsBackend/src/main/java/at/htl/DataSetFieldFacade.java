0f64597ee13cac56ff047f5f767872ceb7e48888
SakalAndrej <andi.sakal15@gmail.com>
2018-01-18 23:19:00 +0100

    refactored project structure

---
+++ HomeDsBackend/src/main/java/at/htl/DataSetFieldFacade.java
@@ -0,0 +1,12 @@
+package at.htl;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.persistence.PersistenceContext;
+
+@ApplicationScoped
+public class DataSetFieldFacade {
+
+
+    @PersistenceContext
+    
+}

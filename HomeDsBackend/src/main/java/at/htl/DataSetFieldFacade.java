6149da909cf24ddbe2848e834ee7e1b919d381d7
SakalAndrej <andi.sakal15@gmail.com>
2018-01-23 12:46:51 +0100

    fixed internationalization, added DataSetFieldFacade

--- HomeDsBackend/src/main/java/at/htl/DataSetFieldFacade.java
+++ HomeDsBackend/src/main/java/at/htl/DataSetFieldFacade.java
@@ -1,6 +1,9 @@
 package at.htl;
 
+import at.htl.model.DataSetDataField;
+
 import javax.faces.bean.ApplicationScoped;
+import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @ApplicationScoped
@@ -8,5 +11,14 @@ public class DataSetFieldFacade {
 
 
     @PersistenceContext
+    EntityManager entityManager;
+
+    public void Save(DataSetDataField dataField) {
+        entityManager.persist(dataField);
+    }
+
+    public DataSetDataField findById(long id) {
+        return entityManager.find(DataSetDataField.class, id);
+    }
     
 }
--- HomeDsBackend/src/main/java/at/htl/web/LanguageController.java
+++ HomeDsBackend/src/main/java/at/htl/web/LanguageController.java
@@ -1,40 +1,15 @@
 package at.htl.web;
 
-import sun.misc.resources.Messages;
-
 import java.io.Serializable;
-import java.lang.reflect.Field;
 import java.util.Locale;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
-import static com.oracle.tools.packager.linux.LinuxDebBundler.BUNDLE_NAME;
-
 @ManagedBean
 @SessionScoped
 public class LanguageController implements Serializable {
-
-    static {
-        // load message values from bundle file
-        final Field[] fieldArray = Messages.class.getDeclaredFields();
-        final int len = fieldArray.length;
-        for (int i = 0; i < len; i++) {
-            final Field field = (Field) fieldArray[i];
-            if (field.getType() == java.lang.String.class) {
-                if (!field.isAccessible())
-                    field.setAccessible(true);
-                try {
-                    final String rawValue = (String) field.get(null);
-                    field.set(null, new String(rawValue.getBytes("ISO-8859-1"),
-                            "UTF-8"));
-                } catch (Exception e) {
-                    // skip field modification
-                }
-            }
-        }
-    }
 
     public void countryLocaleCodeChanged() {
 

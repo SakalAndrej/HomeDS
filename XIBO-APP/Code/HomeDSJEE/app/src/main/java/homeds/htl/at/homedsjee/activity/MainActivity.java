7a86dcd2a34b912b0cf6b8a16f8f278ef29e2646
Unknown <felix.hofmann@liwest.at>
2018-02-06 10:59:30 +0100

    Initial commit

---
+++ XIBO-APP/Code/HomeDSJEE/app/src/main/java/homeds/htl/at/homedsjee/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package homeds.htl.at.homedsjee.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import homeds.htl.at.homedsjee.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

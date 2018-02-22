79790c837ac2454bdd609c328b88122329c69639
SakalAndrej <andi.sakal15@gmail.com>
2018-02-22 16:42:36 +0100

    added custom noConnectionException for better  user experience

---
+++ HomeDsBackend/src/main/java/at/htl/exceptions/NoConnectionException.java
@@ -0,0 +1,19 @@
+package at.htl.exceptions;
+
+public class NoConnectionException extends Exception {
+
+    public NoConnectionException() {
+    }
+
+    public NoConnectionException(String message) {
+        super(message);
+    }
+
+    public NoConnectionException (Throwable cause) {
+        super (cause);
+    }
+
+    public NoConnectionException (String message, Throwable cause) {
+        super (message, cause);
+    }
+}

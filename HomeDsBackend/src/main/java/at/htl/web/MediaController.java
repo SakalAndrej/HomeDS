2f5e7aa9e6c5d34838c7a6604af08a0989773fb3
Unknown <felix.hofmann@liwest.at>
2018-02-28 13:50:02 +0100

    start implementation of media changer

---
+++ HomeDsBackend/src/main/java/at/htl/web/MediaController.java
@@ -0,0 +1,49 @@
+package at.htl.web;
+
+import at.htl.exceptions.NoConnectionException;
+import at.htl.model.Media;
+import at.htl.xiboClient.MediaApi;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Model
+@Named
+public class MediaController  implements Serializable{
+
+
+    @Inject
+    MediaApi mediaApi;
+
+
+    private List<Media> medias;
+
+    @PostConstruct
+    public void init(){
+        try {
+            this.updaeList();
+        } catch (NoConnectionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    private void updaeList() throws NoConnectionException {
+        this.medias = mediaApi.getAllMedia();
+    }
+
+
+    public List<Media> getMedias() {
+        return medias;
+    }
+
+    public void setMedias(List<Media> medias) {
+        this.medias = medias;
+    }
+}
--- HomeDsBackend/src/main/java/at/htl/xiboClient/DataSetApi.java
+++ HomeDsBackend/src/main/java/at/htl/xiboClient/DataSetApi.java
@@ -301,4 +301,6 @@ public class DataSetApi {
             e.printStackTrace();
         }
     }
+
+
 }

959dc2808782c38c5f38f1ce48cb741042db04ae
SakalAndrej <andi.sakal15@gmail.com>
2018-03-01 18:22:11 +0100

    merge

--- HomeDsBackend/src/main/java/at/htl/xiboClient/MediaApi.java
+++ HomeDsBackend/src/main/java/at/htl/xiboClient/MediaApi.java
@@ -3,8 +3,8 @@ package at.htl.xiboClient;
 import at.htl.enums.RequestTypeEnum;
 import at.htl.exceptions.NoConnectionException;
 import at.htl.model.Media;
-import at.htl.xiboClient.helper.AuthentificationHandler;
-import at.htl.xiboClient.helper.RequestHelper;
+import at.htl.utils.AuthentificationHandler;
+import at.htl.utils.RequestHelper;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -20,23 +20,23 @@ import java.util.LinkedList;
 public class MediaApi{
     public final String editWidgetId = "427";
     public final String editPlaylistId = "157";
-    public LinkedList<Media> getAllMedia() throws NoConnectionException{
 
+    public LinkedList<Media> getAllMedia() throws NoConnectionException{
 
         BufferedReader in;
         LinkedList<Media> medias = new LinkedList<Media>();
         Media actual = new Media();
+
         HashMap<String,String> params = new HashMap<>();
         params.put("type","image");
+
         try {
             HttpURLConnection con = new RequestHelper()
                     .executeRequest(RequestTypeEnum.GET, null,
                             new RequestHelper().BASE_URL + "api/library?start=0&length=100",
                             AuthentificationHandler.getTOKEN());
-
             try{
                 in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
             }catch (NullPointerException ex){
                 throw new NoConnectionException("Es ist kein Response vorhanden", ex);
             }
@@ -48,12 +48,9 @@ public class MediaApi{
             }
 
             JSONArray jsonArray = new JSONArray(response.toString());
-            System.out.println(response.toString());
-
 
             for (int i  = 0;i < jsonArray.length();i++){
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-
                 actual.setMediaId(jsonObject.getLong("mediaId"));
                 actual.setMediaType(jsonObject.getString("mediaType"));
                 actual.setName(jsonObject.getString("name"));
@@ -61,32 +58,22 @@ public class MediaApi{
                 medias.add(actual);
                 actual = new Media();
             }
-
-
         }catch(IOException e){
             e.printStackTrace();
         }
-
-
         return medias;
 
     }
-
 
     public void eidtWidget(String mediaId) throws NoConnectionException {
 
         BufferedReader in;
 
-
-
-
-
         try {
             HttpURLConnection con = new RequestHelper()
-                    .executeRequest(RequestTypeEnum.POST, "media=[16]",
+                    .executeRequest(RequestTypeEnum.POST, "media[]=158",
                             new RequestHelper().BASE_URL + "api/playlist/library/assign/"+editPlaylistId,
                             AuthentificationHandler.getTOKEN());
-
             try{
                 in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
@@ -100,16 +87,9 @@ public class MediaApi{
                 response.append(output);
             }
 
-            //JSONArray jsonArray = new JSONArray(response.toString());
-            System.out.println(response.toString());
-
-
-
         }catch(IOException e){
             e.printStackTrace();
         }
-
-
     }
 
 }

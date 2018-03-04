dae0584e5148a982b36939449fc41d49102fc3ab
SakalAndrej <andi.sakal15@gmail.com>
2018-03-05 00:03:20 +0100

    added campaigns

---
+++ HomeDsBackend/src/main/java/at/htl/facades/CampaignFacade.java
@@ -0,0 +1,36 @@
+package at.htl.facades;
+
+import at.htl.model.Campaign;
+import at.htl.model.DataSetDataField;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class CampaignFacade {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void save(Campaign campaign) {
+        entityManager.persist(campaign);
+    }
+
+    public void merge(Campaign campaign) {
+        entityManager.merge(campaign);
+    }
+
+    public Campaign findById(long id) {
+        return entityManager.find(Campaign.class,id);
+    }
+
+    public List<Campaign> getAll() {
+        TypedQuery<Campaign> query = entityManager.createNamedQuery("Campaign.GetAll",Campaign.class);
+        return query.getResultList();
+    }
+
+    public void delete(long id) {
+        entityManager.remove(findById(id));
+    }
+}

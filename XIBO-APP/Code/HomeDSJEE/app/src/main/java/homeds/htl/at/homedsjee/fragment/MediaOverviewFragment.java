e2872d40fa248ac655bf1918e6edf06056ac388f
Unknown <felix.hofmann@liwest.at>
2018-03-04 09:40:09 +0100

    added anviagtion for media

--- XIBO-APP/Code/HomeDSJEE/app/src/main/java/homeds/htl/at/homedsjee/fragment/MediaOverviewFragment.java
+++ XIBO-APP/Code/HomeDSJEE/app/src/main/java/homeds/htl/at/homedsjee/fragment/MediaOverviewFragment.java
@@ -32,7 +32,7 @@ import homeds.htl.at.homedsjee.enumeration.RequestTypeEnum;
  * Use the {@link MediaOverviewFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MediaOverviewFragment extends Fragment {
+public class MediaOverviewFragment extends android.support.v4.app.Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";

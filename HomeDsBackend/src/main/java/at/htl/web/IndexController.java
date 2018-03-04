8147ed4b3954273416459624759065100282845e
SakalAndrej <andi.sakal15@gmail.com>
2018-03-04 16:25:25 +0100

    status problem fixed

--- HomeDsBackend/src/main/java/at/htl/web/IndexController.java
+++ HomeDsBackend/src/main/java/at/htl/web/IndexController.java
@@ -4,12 +4,14 @@ import at.htl.exceptions.NoConnectionException;
 import at.htl.model.Display;
 import at.htl.xiboClient.DisplayApi;
 import at.htl.xiboClient.StatusApi;
+import org.eclipse.persistence.jpa.jpql.parser.DateTime;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.Timer;
 
 @Model
 @Named
@@ -18,7 +20,10 @@ public class IndexController {
     @Inject
     StatusApi statusApi;
 
+    public DateTime
+
     public boolean isServerOnline() {
+
         try {
             return statusApi.getIsOnline();
         } catch (NoConnectionException e) {

c6794809f3de05a796e592440d556492847972ac
SakalAndrej <andi.sakal15@gmail.com>
2018-03-06 14:37:33 +0100

    xibo url changed

--- HomeDsBackend/src/main/java/at/htl/utils/RequestHelper.java
+++ HomeDsBackend/src/main/java/at/htl/utils/RequestHelper.java
@@ -12,7 +12,7 @@ import java.net.URL;
 // Singleton Pattern
 public class RequestHelper {
 
-    public String BASE_URL = "http://vm59.htl-leonding.ac.at:8080/";
+    public String BASE_URL = "172.18.199.120";
 
     public HttpURLConnection executeRequest(RequestTypeEnum executeType, String paramsBody, String url, String TOKEN) {
 

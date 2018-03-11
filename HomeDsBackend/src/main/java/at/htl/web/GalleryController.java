104de01c767bc8fc4f7abe0e8c26a8990e764559
SakalAndrej <andi.sakal15@gmail.com>
2018-03-11 03:15:23 +0100

    showing just image types

--- HomeDsBackend/src/main/java/at/htl/web/GalleryController.java
+++ HomeDsBackend/src/main/java/at/htl/web/GalleryController.java
@@ -1,51 +1,59 @@
 package at.htl.web;
 
-import at.htl.exceptions.NoConnectionException;
-import at.htl.xiboClient.CrawlerApi;
-import org.json.JSONArray;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.tidy.Tidy;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.inject.Inject;
+import javax.enterprise.inject.Model;
 import javax.inject.Named;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-@ApplicationScoped
+@Model
 @Named
 public class GalleryController implements Serializable {
 
     private List<String> images;
 
+    private int slideShowDuration = 15000;
+
     @PostConstruct
-    public void init() throws IOException {
+    public void init(){
+        updateImages();
+    }
+
+    private void updateImages() {
         images = new LinkedList<>();
-        InputStream input = new URL("http://vm59.htl-leonding.ac.at:8080/homeds/img/").openStream();
-        Document document = new Tidy().parseDOM(input, null);
-        NodeList imgs = document.getElementsByTagName("a");
-        List<String> srcs = new ArrayList<String>();
+        InputStream input = null;
+        try {
+            input = new URL("http://vm59.htl-leonding.ac.at:8080/homeds/img/").openStream();
 
-        for (int i = 0; i < imgs.getLength(); i++) {
-            String src = "";
-            if (!(src = imgs.item(i).getAttributes().getNamedItem("href").getNodeValue()).equals("/homeds")) {
-                src = src.replace("/homeds","");
-                images.add(src);
+            Document document = new Tidy().parseDOM(input, null);
+            NodeList imgs = document.getElementsByTagName("a");
+            List<String> srcs = new ArrayList<String>();
+
+            for (int i = 0; i < imgs.getLength(); i++) {
+                String src = "";
+                if (!(src = imgs.item(i).getAttributes().getNamedItem("href").getNodeValue()).equals("/homeds")) {
+                    src = src.replace("/homeds", "");
+                    if (src.toLowerCase().contains(".png")
+                            || src.toLowerCase().contains(".jpg")
+                            || src.toLowerCase().contains(".jpeg")
+                            || src.toLowerCase().contains(".ico")
+                            || src.toLowerCase().contains(".gif")
+                            || src.toLowerCase().contains(".bmp")) {
+                        images.add(src);
+                    }
+                }
             }
-        }
-
-        for (String src: srcs) {
-            System.out.println(src);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -53,4 +61,15 @@ public class GalleryController implements Serializable {
         return images;
     }
 
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public int getSlideShowDuration() {
+        return slideShowDuration;
+    }
+
+    public void setSlideShowDuration(int slideShowDuration) {
+        this.slideShowDuration = slideShowDuration;
+    }
 }

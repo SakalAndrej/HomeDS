6f43cf617af314758c3120b510a773f9ff618b4f
SakalAndrej <andi.sakal15@gmail.com>
2018-03-11 02:35:34 +0100

    try out gallery on server

--- HomeDsBackend/src/main/java/at/htl/web/GalleryController.java
+++ HomeDsBackend/src/main/java/at/htl/web/GalleryController.java
@@ -3,6 +3,9 @@ package at.htl.web;
 import at.htl.exceptions.NoConnectionException;
 import at.htl.xiboClient.CrawlerApi;
 import org.json.JSONArray;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.tidy.Tidy;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
@@ -10,8 +13,13 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @ApplicationScoped
@@ -21,9 +29,22 @@ public class GalleryController implements Serializable {
     private List<String> images;
 
     @PostConstruct
-    public void init() {
-        images = new ArrayList<String>();
-        images.add("HomesLogoTrans.png");
+    public void init() throws IOException {
+        images = new LinkedList<>();
+        InputStream input = new URL("http://vm59.htl-leonding.ac.at:8080/img/").openStream();
+        Document document = new Tidy().parseDOM(input, null);
+        NodeList imgs = document.getElementsByTagName("a");
+        List<String> srcs = new ArrayList<String>();
+
+        for (int i = 0; i < imgs.getLength(); i++) {
+            String src = "";
+            if (!(src = imgs.item(i).getAttributes().getNamedItem("href").getNodeValue()).equals("/"))
+                images.add(src);
+        }
+
+        for (String src: srcs) {
+            System.out.println(src);
+        }
     }
 
     public List<String> getImages() {

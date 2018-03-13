952acdafec715c86eed95015c32227f1299f9d1b
Unknown <felix.hofmann@liwest.at>
2018-03-13 20:13:42 +0100

    Display Adapter

--- XIBO-APP/Code/HomeDSJEE/app/src/main/java/homeds/htl/at/homedsjee/adapter/DisplayAdapter.java
+++ XIBO-APP/Code/HomeDSJEE/app/src/main/java/homeds/htl/at/homedsjee/adapter/DisplayAdapter.java
@@ -1,10 +1,13 @@
 package homeds.htl.at.homedsjee.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.LinkedList;
 
+import homeds.htl.at.homedsjee.R;
 import homeds.htl.at.homedsjee.entity.Display;
 import homeds.htl.at.homedsjee.viewholder.DisplayViewHolder;
 
@@ -19,12 +22,13 @@ public class DisplayAdapter extends RecyclerView.Adapter<DisplayViewHolder> {
 
     @Override
     public DisplayViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.display_list_item,parent,false);
+        return new DisplayViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(DisplayViewHolder holder, int position) {
-
+        holder.updateUi(displays.get(position));
     }
 
     @Override

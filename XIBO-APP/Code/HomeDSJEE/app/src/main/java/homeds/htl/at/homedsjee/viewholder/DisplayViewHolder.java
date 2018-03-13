4a812f02d3de86be2393e6ccdeefd1e7a270a15a
Unknown <felix.hofmann@liwest.at>
2018-03-13 20:14:08 +0100

    display viewholder

--- XIBO-APP/Code/HomeDSJEE/app/src/main/java/homeds/htl/at/homedsjee/viewholder/DisplayViewHolder.java
+++ XIBO-APP/Code/HomeDSJEE/app/src/main/java/homeds/htl/at/homedsjee/viewholder/DisplayViewHolder.java
@@ -2,6 +2,12 @@ package homeds.htl.at.homedsjee.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import homeds.htl.at.homedsjee.R;
+import homeds.htl.at.homedsjee.activity.MainActivityBottomNavigation;
+import homeds.htl.at.homedsjee.entity.Display;
 
 /**
  * Created by Felix on 13.03.2018.
@@ -9,8 +15,24 @@ import android.view.View;
 
 public class DisplayViewHolder extends RecyclerView.ViewHolder {
 
+    TextView tvDisplayName;
+    Button btCooseDisplay;
 
     public DisplayViewHolder(View itemView) {
         super(itemView);
+        tvDisplayName = itemView.findViewById(R.id.tvDisplayName);
+        btCooseDisplay = itemView.findViewById(R.id.btChooseDisplay);
+    }
+
+    public void updateUi(final Display display) {
+        tvDisplayName.setText(display.getDisplay());
+        btCooseDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivityBottomNavigation.getInstance().displayId = display.getDisplayId();
+                MainActivityBottomNavigation.getInstance().onBackPressed();
+            }
+        });
+
     }
 }
